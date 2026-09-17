46522a0a760126caeaca89f13a28ecb93349ec06
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-17 15:29:55 +0800

    Create Two Simple dummy Tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 extern "C" {
     #include <esp_log.h>
 }
+#include <FreeRTOS/FreeRTOS.h>
+#include <freeRTOS/task.h>
+
+
+
  const char *MAIN_MONITOR_TAG = "MAIN_MONITOR";
+
+void taskA(void *pvParameters) {
+    while(true) {
+        ESP_LOGI(MAIN_MONITOR_TAG, "Task A is running.");
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+    }
+}
+
+void taskB(void *pvParameters) {
+    while(true) {
+        ESP_LOGI(MAIN_MONITOR_TAG, "Task B is running.");
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+    }
+}
 
 extern "C" void app_main() {
     ESP_LOGI(MAIN_MONITOR_TAG, "\nBCA152 FreeRTOS Multisensor Monitor\nSYSTEM STARTING...");
+    xTaskCreate(taskA, "Dummy Task A", 2048, NULL, 1, NULL);
+    xTaskCreate(taskB, "Dummy Task B", 2048, NULL, 1, NULL);
 }
