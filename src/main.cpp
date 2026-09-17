a0b723186319eee08fb8eb5d4cbcffe105ec8705
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-18 00:07:18 +0800

    Updating DHT22 Circuit and Creating task for DHT22 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,29 @@ extern "C" {
 }
 #include <FreeRTOS/FreeRTOS.h>
 #include <freeRTOS/task.h>
+#include "sensors.h"
 
+const char *MAIN_MONITOR_TAG = "MAIN_MONITOR";
 
+void sensorTask(void *pvParameters) {
+    Pin_DHT22_Init(GPIO_NUM_18); // Initialize DHT22 sensor on GPIO 18
+    float temperature = 0.0f;
+    float humidity = 0.0f;
+    
 
- const char *MAIN_MONITOR_TAG = "MAIN_MONITOR";
-
-void taskA(void *pvParameters) {
     while(true) {
-        ESP_LOGI(MAIN_MONITOR_TAG, "Task A is running.");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
-    }
-}
-
-void taskB(void *pvParameters) {
-    while(true) {
-        ESP_LOGI(MAIN_MONITOR_TAG, "Task B is running.");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+        esp_err_t result = DHT22_send_receive_data(&temperature, &humidity);
+        if(result == ESP_OK) {
+            ESP_LOGI(MAIN_MONITOR_TAG, "Temperature: %.2f°C, Humidity: %.2f%%", temperature, humidity);
+        } else {
+            ESP_LOGE(MAIN_MONITOR_TAG, "Failed to read from DHT22 sensor. Error code: %d", result);
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000)); // Delay for 2 seconds
     }
 }
 
 extern "C" void app_main() {
     ESP_LOGI(MAIN_MONITOR_TAG, "\nBCA152 FreeRTOS Multisensor Monitor\nSYSTEM STARTING...");
-    xTaskCreate(taskA, "Dummy Task A", 2048, NULL, 1, NULL);
-    xTaskCreate(taskB, "Dummy Task B", 2048, NULL, 1, NULL);
+    xTaskCreate(sensorTask, "Sensor Task", 4096, NULL, 1, NULL);
 }
+
