1fe0b829aaaec131ebf34f2069e1b731f3ab0ab8
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-18 01:35:08 +0800

    Adding LDR sensor diagram and combining sensor tasks

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,25 @@ void sensorTask(void *pvParameters) {
     float temperature = 0.0f;
     float humidity = 0.0f;
     
+    Pin_LDR_Init(); // Initialize LDR sensor on ADC channel 8
+    float percent = 0.0f; // Variable to store the percentage of light intensity
+    int raw_value = 0; // Variable to store the raw ADC value from the LDR sensor
 
     while(true) {
         esp_err_t result = DHT22_send_receive_data(&temperature, &humidity);
         if(result == ESP_OK) {
-            ESP_LOGI(MAIN_MONITOR_TAG, "Temperature: %.2f°C, Humidity: %.2f%%", temperature, humidity);
+            ESP_LOGI(MAIN_MONITOR_TAG, "Temperature: %.2f°C, Humidity: %.2f%%", temperature, humidity); //  Log the temperature and humidity values from the DHT22 sensor
         } else {
-            ESP_LOGE(MAIN_MONITOR_TAG, "Failed to read from DHT22 sensor. Error code: %d", result);
+            ESP_LOGE(MAIN_MONITOR_TAG, "Failed to read from DHT22 sensor. Error code: %d", result); // Log an error message if reading from the DHT22 sensor fails
         }
+
+        esp_err_t ldr_result = LDR_receive_data(&percent, &raw_value); // Read data from LDR sensor
+        if(ldr_result == ESP_OK) {
+            ESP_LOGI(MAIN_MONITOR_TAG, "Percent: %.2f%% (Raw: %d)", percent, raw_value); // Log the percentage and raw value from the LDR sensor
+        } else {
+            ESP_LOGE(MAIN_MONITOR_TAG, "Failed to read from LDR sensor. Error code: %d", ldr_result); // Log an error message if reading from the LDR sensor fails  
+        }
+
         vTaskDelay(pdMS_TO_TICKS(2000)); // Delay for 2 seconds
     }
 }
