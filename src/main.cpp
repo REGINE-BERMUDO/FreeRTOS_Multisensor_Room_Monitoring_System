0307d5accd3c97275754708ee8c5de707200dc15
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-18 16:07:35 +0800

    Replacing vTaskDelay into vTaskDelayUntil for Sensor Tasks.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 const char *MAIN_MONITOR_TAG = "MAIN_MONITOR";
 
 void sensorTask(void *pvParameters) {
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
     Initialize_DHT22_LDR_PINS(); // Initialize DHT22 and LDR sensor pins
     float temperature = 0.0f;
     float humidity = 0.0f;
@@ -11,11 +13,12 @@ void sensorTask(void *pvParameters) {
     float percent = 0.0f; // Variable to store the percentage of light intensity
     int raw_value = 0; // Variable to store the raw ADC value from the LDR sensor
 
+
     while(true) {
         DHT22_Print(&temperature, &humidity);
         LDR_Print(&percent, &raw_value);
 
-        vTaskDelay(pdMS_TO_TICKS(2000)); // Delay for 2 seconds
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(2000)); // Delay for 2 seconds
     }
 }
 
