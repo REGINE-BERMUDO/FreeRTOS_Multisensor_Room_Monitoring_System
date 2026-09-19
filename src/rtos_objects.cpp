a08e069b158cb5ce67d9208ce7305b17b5fb077b
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-19 15:30:32 +0800

    Initialize Display Task for OLED

--- src/rtos_objects.cpp
+++ src/rtos_objects.cpp
@@ -5,7 +5,7 @@ QueueHandle_t sensorQueue;
 
 void Initialize_FreeRTOS_Tasks(void) {
     xTaskCreate(SensorTask, "Sensor Task", 4096, NULL, 1, NULL);
-    //xTaskCreate(DisplayTask, "Display Task", 4096, NULL, 1, NULL); 
+    xTaskCreate(DisplayTask, "Display Task", 4096, NULL, 1, NULL); 
 }
 
 esp_err_t Initialize_FreeRTOS_Queues(void) {
