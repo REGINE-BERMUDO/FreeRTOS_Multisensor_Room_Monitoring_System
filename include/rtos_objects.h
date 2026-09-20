5d25aa507d749603d4d90e9f88ac156eefbb5892
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-20 18:10:28 +0800

    Adding Event Group state

--- include/rtos_objects.h
+++ include/rtos_objects.h
@@ -4,6 +4,10 @@
 #define SENSORQUEUELENGTH 5
 #define ALARMQUEUELENGTH 5
 
+#define EVENT_ACTIVE BIT0 
+#define EVENT_MOTION BIT1
+#define EVENT_ALARM BIT2
+
 #include <freeRTOS/FreeRTOS.h>
 #include <freeRTOS/task.h>
 #include "sensors.h"
@@ -11,15 +15,19 @@
 // FreeRTOS Queue Handlers
 extern QueueHandle_t sensorQueue;
 extern QueueHandle_t alarmQueue; 
+extern EventGroupHandle_t stateEventGroup; // Event group to manage system states
 
 // FreeRTOS Task Function Prototypes
 void SensorTask(void *pvParameters);
 void DisplayTask(void *pvParameters);
 void InputTask(void *pvParameters); 
 void AlarmTask(void *pvParameters);
+void MotionTask(void *pvParameters);
+
 
 // FreeRTOS Queue & Tasks Initialization
 void Initialize_FreeRTOS_Tasks(void);
 esp_err_t Initialize_FreeRTOS_Queues(void);
+esp_err_t Initialize_FreeRTOS_Semaphore_Event(void);
 
 #endif
--- src/rtos_objects.cpp
+++ src/rtos_objects.cpp
@@ -3,12 +3,15 @@
 // FreeRTOS Queue Handlers
 QueueHandle_t sensorQueue;
 QueueHandle_t alarmQueue;
+EventGroupHandle_t stateEventGroup; // Event group to manage system states
 
-void Initialize_FreeRTOS_Tasks(void) {
+
+void Initialize_FreeRTOS_Tasks(void){
     xTaskCreate(SensorTask, "Sensor Task", 4096, NULL, 2, NULL);
     xTaskCreate(DisplayTask, "Display Task", 4096, NULL, 1, NULL);
     xTaskCreate(InputTask, "Input Task", 2048, NULL, 3, NULL);
-    xTaskCreate(AlarmTask, "Alarm Task", 2048, NULL, 4, NULL); 
+    xTaskCreate(AlarmTask, "Alarm Task", 2048, NULL, 2, NULL); 
+    xTaskCreate(MotionTask, "Motion Task", 2048, NULL, 3, NULL);
 }
 
 esp_err_t Initialize_FreeRTOS_Queues(void) {
@@ -24,3 +27,13 @@ esp_err_t Initialize_FreeRTOS_Queues(void) {
     }
     return ESP_OK;
 }
+
+esp_err_t Initialize_FreeRTOS_Semaphore_Event(void){
+    stateEventGroup = xEventGroupCreate();
+    if(stateEventGroup == nullptr) {
+        return ESP_ERR_TIMEOUT;
+    }
+    xEventGroupSetBits(stateEventGroup, EVENT_ACTIVE); 
+    return ESP_OK;
+}
+   
