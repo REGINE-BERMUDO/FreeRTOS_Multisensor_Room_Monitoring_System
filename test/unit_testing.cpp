a1cdafddf3850eaed1a59df4c0df6ef91d1316f7
unknown <maregine.bermudo@g.msuiit.edu.ph>
2026-09-21 21:06:03 +0800

    Adding System State test to Unit testing

--- test/unit_testing.cpp
+++ test/unit_testing.cpp
@@ -1,6 +1,7 @@
 #include <unity.h>
 #include "alarm.h"
-#include "display.h"    
+#include "display.h"
+#include "system_state.h"    
 
 void setUp(void);
 void tearDown(void);
@@ -30,8 +31,7 @@ void temp_above_upper_threshold(void) {
     TEST_ASSERT_EQUAL(AlarmState::HIGH_TEMPERATURE, evaluateTemperature(35.0f));
 }
 
-
-
+// Display update tests for clockwise and counterclockwise rotations
 void display_test_clockwise_temp(void) {
     SensorData display_dataReceived = {25.0f, 50.0f, 300, true}; // Example sensor data
     char data_print[15];
@@ -87,6 +87,28 @@ void display_test_counterclockwise_temp(void) {
     Display_Process_Update(&display_dataReceived, (uint8_t)DisplayMode::TEMPERATURE, data_print, sizeof(data_print));
 }
 
+// System state evaluation tests
+void system_active_notimeout(void){
+    bool is_active = evaluate_State(true, false, 1000);
+    TEST_ASSERT_TRUE(is_active);
+}
+
+void system_active_timeout(void){
+    bool is_active = evaluate_State(true, false, 20000);
+    TEST_ASSERT_FALSE(is_active);
+}
+
+void system_inactive_nomotion(void){
+    bool is_active = evaluate_State(false, false, 1000);
+    TEST_ASSERT_FALSE(is_active);
+}
+
+void system_inactive_motion(void){
+    bool is_active = evaluate_State(false, true, 1000);
+    TEST_ASSERT_TRUE(is_active);
+}
+
+// Main function to run all tests
 extern "C" void app_main(void){
     UNITY_BEGIN();
 
@@ -109,6 +131,12 @@ extern "C" void app_main(void){
     RUN_TEST(display_test_counterclockwise_light);
     RUN_TEST(display_test_counterclockwise_humidity);
     RUN_TEST(display_test_counterclockwise_temp);
-    
+
+    // System state evaluation tests
+    RUN_TEST(system_active_notimeout);
+    RUN_TEST(system_active_timeout);
+    RUN_TEST(system_inactive_nomotion);
+    RUN_TEST(system_inactive_motion);
+
     UNITY_END();
 }
